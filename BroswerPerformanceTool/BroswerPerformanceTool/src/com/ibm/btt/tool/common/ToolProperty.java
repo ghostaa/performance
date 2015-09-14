4641d705ce57fb4d570b6c21213271c7cd27da41
ydp <ydp@f164b24f-02d4-4148-90b1-c971cc8a6458>
2015-09-14 11:53:27 +0000

    git-svn-id: file:///c/Repositories/performance@16 f164b24f-02d4-4148-90b1-c971cc8a6458

---
+++ BroswerPerformanceTool/BroswerPerformanceTool/src/com/ibm/btt/tool/common/ToolProperty.java
@@ -0,0 +1,7 @@
+package com.ibm.btt.tool.common;
+
+public class ToolProperty {
+	public static int totalTimes; //总次数(总共记录多少次数据)
+	public static int recordInterval;//记录间隔(点击多少次记录一次)
+	public static int waitTime;//等待时间(多少秒点击一次)
+}
