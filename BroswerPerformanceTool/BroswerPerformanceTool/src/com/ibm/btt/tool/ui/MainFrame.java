1025590017b5a9ce1dc2a56a3501c0da09aaaa81
ydp <ydp@f164b24f-02d4-4148-90b1-c971cc8a6458>
2015-11-11 10:14:59 +0000

    提交界面雏形
    git-svn-id: file:///c/Repositories/performance@17 f164b24f-02d4-4148-90b1-c971cc8a6458

---
+++ BroswerPerformanceTool/BroswerPerformanceTool/src/com/ibm/btt/tool/ui/MainFrame.java
@@ -0,0 +1,68 @@
+package com.ibm.btt.tool.ui;
+
+import java.awt.BorderLayout;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JLabel;
+import javax.swing.JMenuItem;
+import java.awt.SystemColor;
+
+public class MainFrame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MainFrame() {
+		setTitle("BTT Broswer Performance Tool");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("New menu");
+		menuBar.add(mnNewMenu);
+		
+		JMenu mnNewMenu_1 = new JMenu("New menu");
+		mnNewMenu.add(mnNewMenu_1);
+		
+		JMenu mnNewMenu_2 = new JMenu("New menu");
+		menuBar.add(mnNewMenu_2);
+		
+		JMenu mnNewMenu_3 = new JMenu("Help");
+		menuBar.add(mnNewMenu_3);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("About");
+		mnNewMenu_3.add(mntmNewMenuItem);
+		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.info);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+	}
+
+}
